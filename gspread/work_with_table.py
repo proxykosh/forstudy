a1d1bda07e327e6b2c10292fe0a1c7efb94526c0
proxykosh <muhidimakzn@gmail.com>
2021-12-23 15:47:08 +0300

    test add VE

--- gspread/work_with_table.py
+++ gspread/work_with_table.py
@@ -12,8 +12,25 @@ client = gspread.authorize(creds)
 
 google_sh = client.open_by_url("https://docs.google.com/spreadsheets/d/1SJr8wRfYufQKFIk3pKyqATvAR86Mu6yBcT7CzZREq5M/edit#gid=0")
 worksheet = google_sh.get_worksheet(0)
-values_list = worksheet.row_values(1)
-print(values_list)
+df = pd.DataFrame(data=worksheet.get_all_values())
+df = df.rename(columns=df.iloc[0]).drop(df.index[0])
+insertRow = ['Test', 'Test', 'Test', 'Test','Test', 'Test']
+# worksheet.append_row(insertRow, table_range="B2:D7")
+# worksheet.delete_rows(68)
+
+
+cell_list = worksheet.range('A2:G2')
+cell_values = [1, 2, 3, 4, 5, 6, 7]
+
+for i, val in enumerate(cell_values):  #gives us a tuple of an index and value
+    cell_list[i].value = val    #use the index on cell_list and the val from cell_values
+
+
+worksheet.update_cells(cell_list)
+
+worksheet.delete_rows(2,80)
+worksheet.delete_columns(1, 13)
+
 
 
 
